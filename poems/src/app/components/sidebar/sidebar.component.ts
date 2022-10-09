e4ea82e3506e6816e611a5e53025c22e0cf6afdd
Breejesh Rathod <breejesh2212@gmail.com>
2022-10-09 14:53:58 +0530

    nav, sidebar, content responsivness change

--- poems/src/app/components/sidebar/sidebar.component.ts
+++ poems/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  showSidebar = true;
+  isSmallerScreen = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.initSizeVariables();
   }
 
+  initSizeVariables() {
+    if (window.screen.width < 1200) {
+      this.showSidebar = false;
+      this.isSmallerScreen = true;
+    } else {
+      this.showSidebar = true;
+      this.isSmallerScreen = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.initSizeVariables();
+  }
+
+  showSidebarClick() {
+    this.showSidebar = true;
+  }
+
+  hideSidebarClick() {
+    this.showSidebar = false;
+  }
 }
